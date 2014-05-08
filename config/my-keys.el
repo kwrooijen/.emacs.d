e0d131855fe3c4940dac18d9bdc433e7825cf4c3
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2014-05-08 23:50:48 +0200

    added attic mode again

--- config/my-keys.el
+++ config/my-keys.el
@@ -1,3 +1,4 @@
+(defvar attic-minor-mode-map (make-keymap) "attic-minor-mode keymap.")
 (defvar insert-mode-map (make-keymap) "insert-mode keymap.")
 
 (key-chord-define-global "qq" 'god-mode-enable)
@@ -7,59 +8,59 @@
                                 (save-buffer)))
 
 ;; Control Keys
-(define-key god-local-mode-map (kbd "C-u") 'pop-to-mark-command)
-(define-key god-local-mode-map (kbd "C-q") 'backward-delete-char)
-(define-key god-local-mode-map (kbd "C--") 'undo)
-(define-key god-local-mode-map (kbd "C-l") 'iy-go-up-to-char)
-(define-key god-local-mode-map (kbd "C-h") 'helm-M-x)
-(define-key god-local-mode-map (kbd "C-z") 'helm-buffers-list)
-(define-key god-local-mode-map (kbd "C-.") 'helm-resume)
-(define-key god-local-mode-map (kbd "C-j") 'ace-jump-mode)
-(define-key god-local-mode-map (kbd "C-/") 'comment-or-uncomment-region)
+(define-key attic-minor-mode-map (kbd "C-u") 'pop-to-mark-command)
+(define-key attic-minor-mode-map (kbd "C-q") 'backward-delete-char)
+(define-key attic-minor-mode-map (kbd "C--") 'undo)
+(define-key attic-minor-mode-map (kbd "C-l") 'iy-go-up-to-char)
+(define-key attic-minor-mode-map (kbd "C-h") 'helm-M-x)
+(define-key attic-minor-mode-map (kbd "C-z") 'helm-buffers-list)
+(define-key attic-minor-mode-map (kbd "C-.") 'helm-resume)
+(define-key attic-minor-mode-map (kbd "C-j") 'ace-jump-mode)
+(define-key attic-minor-mode-map (kbd "C-/") 'comment-or-uncomment-region)
 
 ;; Control Prefix
-(define-key god-local-mode-map (kbd "C-x C-f") 'helm-find-files)
-(define-key god-local-mode-map (kbd "C-c C-f") 'helm-ls-git-ls)
-(define-key god-local-mode-map (kbd "C-c C-o") 'hoogle-search)
-(define-key god-local-mode-map (kbd "C-c C-t") 'transpose-paragraphs)
-(define-key god-local-mode-map (kbd "C-c C-m") 'magit-status)
-(define-key god-local-mode-map (kbd "C-c C-q") 'kmacro-start-macro)
-(define-key god-local-mode-map (kbd "C-c C-e") 'kmacro-end-or-call-macro-repeat)
-(define-key god-local-mode-map (kbd "C-x C-1") 'delete-other-windows)
-(define-key god-local-mode-map (kbd "C-x C-2") 'split-window-below)
-(define-key god-local-mode-map (kbd "C-x C-3") 'split-window-right)
-(define-key god-local-mode-map (kbd "C-x C-0") 'delete-window)
+(define-key attic-minor-mode-map (kbd "C-x C-f") 'helm-find-files)
+(define-key attic-minor-mode-map (kbd "C-c C-f") 'helm-ls-git-ls)
+(define-key attic-minor-mode-map (kbd "C-c C-o") 'hoogle-search)
+(define-key attic-minor-mode-map (kbd "C-c C-t") 'transpose-paragraphs)
+(define-key attic-minor-mode-map (kbd "C-c C-m") 'magit-status)
+(define-key attic-minor-mode-map (kbd "C-c C-q") 'kmacro-start-macro)
+(define-key attic-minor-mode-map (kbd "C-c C-e") 'kmacro-end-or-call-macro-repeat)
+(define-key attic-minor-mode-map (kbd "C-x C-1") 'delete-other-windows)
+(define-key attic-minor-mode-map (kbd "C-x C-2") 'split-window-below)
+(define-key attic-minor-mode-map (kbd "C-x C-3") 'split-window-right)
+(define-key attic-minor-mode-map (kbd "C-x C-0") 'delete-window)
 
 ;; Control Prefix 3
-(define-key god-local-mode-map (kbd "C-c C-w C-e") 'load-attic-workgroups)
-(define-key god-local-mode-map (kbd "C-c C-w C-w") 'load-attic-workgroups2)
-(define-key god-local-mode-map (kbd "C-c C-s C-g") 'helm-do-grep)
-(define-key god-local-mode-map (kbd "C-c C-s C-s") 'helm-swoop)
-(define-key god-local-mode-map (kbd "C-c C-s C-m") 'helm-multi-swoop)
-(define-key god-local-mode-map (kbd "C-c C-s C-r") (lambda () (interactive)
+(define-key attic-minor-mode-map (kbd "C-c C-w C-e") 'load-attic-workgroups)
+(define-key attic-minor-mode-map (kbd "C-c C-w C-w") 'load-attic-workgroups2)
+(define-key attic-minor-mode-map (kbd "C-c C-s C-g") 'helm-do-grep)
+(define-key attic-minor-mode-map (kbd "C-c C-s C-s") 'helm-swoop)
+(define-key attic-minor-mode-map (kbd "C-c C-s C-m") 'helm-multi-swoop)
+(define-key attic-minor-mode-map (kbd "C-c C-s C-r") (lambda () (interactive)
     (let ((current-prefix-arg '(1)))
       (call-interactively 'helm-do-grep))))
 
 ;; Make keys
-(define-key god-local-mode-map (kbd "C-c C-z C-z") 'run-make)
-(define-key god-local-mode-map (kbd "C-c C-z C-j") 'run-make-js)
-(define-key god-local-mode-map (kbd "C-c C-z C-s") 'run-make-start)
-(define-key god-local-mode-map (kbd "C-c C-z C-r") 'run-make-restart)
+(define-key attic-minor-mode-map (kbd "C-c C-z C-z") 'run-make)
+(define-key attic-minor-mode-map (kbd "C-c C-z C-j") 'run-make-js)
+(define-key attic-minor-mode-map (kbd "C-c C-z C-s") 'run-make-start)
+(define-key attic-minor-mode-map (kbd "C-c C-z C-r") 'run-make-restart)
 
 ;; Meta Keys
-(define-key god-local-mode-map (kbd "M-q") 'backward-kill-word)
-(define-key god-local-mode-map (kbd "M-+") 'align-regexp)
-(define-key god-local-mode-map (kbd "M-@") 'er/expand-region)
-(define-key god-local-mode-map (kbd "M-x") nil)
-(define-key god-local-mode-map (kbd "M-k") 'kill-this-buffer)
-(define-key god-local-mode-map (kbd "M-P") 'mc/mark-previous-like-this)
-(define-key god-local-mode-map (kbd "M-N") 'mc/mark-next-like-this)
-(define-key god-local-mode-map (kbd "M-*") 'mc/mark-all-like-this)
-(define-key god-local-mode-map (kbd "M-_") 'redo)
-(define-key god-local-mode-map (kbd "M-C-_") 'redo)
-(define-key god-local-mode-map (kbd "M-C--") 'redo)
-(define-key god-local-mode-map (kbd "M-j") (lambda() (interactive) (join-line -1)))
-(define-key god-local-mode-map (kbd "M-g") 'goto-line)
+(define-key attic-minor-mode-map (kbd "M-q") 'backward-kill-word)
+(define-key attic-minor-mode-map (kbd "M-+") 'align-regexp)
+(define-key attic-minor-mode-map (kbd "M-@") 'er/expand-region)
+(define-key attic-minor-mode-map (kbd "M-x") nil)
+(define-key attic-minor-mode-map (kbd "M-k") 'kill-this-buffer)
+(define-key attic-minor-mode-map (kbd "M-P") 'mc/mark-previous-like-this)
+(define-key attic-minor-mode-map (kbd "M-N") 'mc/mark-next-like-this)
+(define-key attic-minor-mode-map (kbd "M-*") 'mc/mark-all-like-this)
+(define-key attic-minor-mode-map (kbd "M-_") 'redo)
+(define-key attic-minor-mode-map (kbd "M-C-_") 'redo)
+(define-key attic-minor-mode-map (kbd "M-C--") 'redo)
+(define-key attic-minor-mode-map (kbd "M-j") (lambda() (interactive) (join-line -1)))
+(define-key attic-minor-mode-map (kbd "M-g") 'goto-line)
 (define-key minibuffer-local-map (kbd "M-g") 'keyboard-escape-quit)
 (define-key mc/keymap (kbd "M-g") 'keyboard-escape-quit-mc)
 
@@ -93,10 +94,17 @@
 (define-key god-local-mode-map (kbd "]") (lambda ()
     (interactive) (scroll-up-line 3)))
 
+;; Modes
+(define-minor-mode attic-minor-mode
+"A minor mode so that my key settings override annoying major modes."
+
+t " attic" 'attic-minor-mode-map)
+(defun attic-minibuffer-setup-hook ()
+(attic-minor-mode 0))
+
 ; Disable Control keys in insert mode
 (defvar insert-minor-mode-map (make-keymap)
   "insert-minor-mode keymap.")
-
 
 (let ((f (lambda () `(lambda () (interactive)
               (message (concat "Exit insert mode first."))))))
@@ -107,7 +115,6 @@
                ("C-b") ("C-n") ("C-m")))
     (define-key insert-minor-mode-map
       (read-kbd-macro (car l)) (funcall f))))
-
 
 (define-minor-mode insert-minor-mode
   "Insert mode" nil nil 'insert-minor-mode-map)
