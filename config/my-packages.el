bafa6bd81c448f8559940790ba421f4fa4ed35d5
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2014-05-18 17:53:40 +0200

    Added packages

--- config/my-packages.el
+++ config/my-packages.el
@@ -4,29 +4,31 @@
   (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
   )
 
-(defvar my-packages
-  '( ace-jump-mode
-     auto-complete
-     erlang
-     expand-region
-     god-mode
-     haskell-mode
-     helm
-     helm-ls-git
-     helm-swoop
-     iy-go-to-char
-     js2-mode
-     key-chord
-     magit
-     multiple-cursors
-     rainbow-delimiters
-     redo+
-     powerline
-     web-mode
-     window-numbering
-     wrap-region
-     yasnippet
-   ))
+(defvar my-packages '(
+    ace-jump-mode
+    auto-complete
+    emms
+    erlang
+    expand-region
+    god-mode
+    hackernews
+    haskell-mode
+    helm
+    helm-ls-git
+    helm-swoop
+    iy-go-to-char
+    js2-mode
+    key-chord
+    magit
+    multiple-cursors
+    powerline
+    rainbow-delimiters
+    redo+
+    web-mode
+    window-numbering
+    wrap-region
+    yasnippet
+    ))
 
 (defun my-install-packages ()
   (interactive)
