494e27c1a40c4a514ad92c336e74b414b8a218af
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2014-05-18 17:45:26 +0200

    Added comments and made VLC default player

--- config/my-extras.el
+++ config/my-extras.el
@@ -1,13 +1,25 @@
 (require 'emms-setup)
 
+;; Don't use mplayer, after suspend it crashes and violates your ears
+(setq emms-setup-default-player-list '(emms-player-vlc))
+
+;; Default config for emms
 (emms-standard)
 (emms-default-players)
-(emms-toggle-repeat-playlist)
+
+;; Use the Music directory as default playlist
 (emms-add-directory-tree "~/Music")
+
+;; Toggle repeat and shuffle Music playlist
+(emms-toggle-repeat-playlist)
 (emms-shuffle)
 
+;; Adjust volume by 10 on change
 (setq emms-volume-change-amount 10)
 
+;; List of commands for emms "C-c C-a C-?"
+;; These command can be repeated by pressing the last
+;; pressed key thanks to command-repeater
 (define-key attic-minor-mode-map (kbd "C-c C-a")
   (lambda () (interactive) (command-repeater '(
     ("a" . emms-pause)
