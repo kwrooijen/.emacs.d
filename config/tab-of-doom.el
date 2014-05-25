f0e056ce104e92d5953f8d75214ef401e84c95de
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2014-05-25 23:17:57 +0200

    Tab of doom indents lists properly now

--- config/tab-of-doom.el
+++ config/tab-of-doom.el
@@ -6,36 +6,41 @@
     "One tabbing mode to rule them all"
     nil " ToD" 'tab-of-doom-mode-map)
 
+(defun go-back-with-result (function)
+    "Run a function and return it's result.
+    And return the cursor to it's original position"
+    (let ((previous (point)))
+        (funcall function)
+        (let ((result (current-column)))
+            (goto-char previous)
+        result)))
+
+(defun go-back-with-result-2 (function function2)
+    "Run a function and return it's result.
+    And return the cursor to it's original position"
+    (let ((previous (point)))
+        (let ((result (funcall function2)))
+            (goto-char previous)
+        result)))
+
 (defun line-length ()
     "This function will return the length of a line"
-    (let ((current (current-column)))
-        (end-of-line) (current-column)
-        (let ((end (current-column)))
-            (move-to-column current)
-            end)))
+    (go-back-with-result (lambda ()
+        (end-of-line) (current-column))))
 
 (defun get-previous-indent ()
     "Get the column number of the previous line's indent point"
-    (let ((previous (point)))
+    (go-back-with-result (lambda()
         (previous-line)
         (end-of-line)
         (while (equal (current-column) 0)
-            (progn
-                (previous-line)
-                (end-of-line)
-            ))
-        (back-to-indentation)
-        (let ((result (current-column)))
-            (goto-char previous)
-            result)))
+            (progn (previous-line) (end-of-line)))
+        (back-to-indentation))))
 
 (defun get-current-indent ()
     "Get the column number of the current line's indent point"
-    (let ((previous (current-column)))
-        (back-to-indentation)
-        (let ((result (current-column)))
-            (move-to-column previous)
-            result)))
+    (go-back-with-result (lambda ()
+        (back-to-indentation))))
 
 (defun take-to-column (col)
     "Move line to column number"
@@ -44,10 +49,21 @@
     (just-one-space 0)
     (let (c) (-dotimes col (lambda (n) (insert " "))) c))
 
+(defun prev-end-is (x)
+    (equal x (go-back-with-result-2 (lambda ()
+        (previous-line)
+        (end-of-line)
+        (while (equal (current-column) 0)
+            (progn
+                (previous-line)
+                (end-of-line)
+                ))
+            (backward-char))
+            (lambda () (string (char-after (point)))))))
+
 (defun tab-of-doom-region ()
     "Tab of Doom for region selection"
     (return))
-
 (defun tab-of-doom-mc ()
     "Tab of Doom for multiple cursors"
     (return))
@@ -62,11 +78,14 @@
         (curr-ind    (get-current-indent))
         (prev-length (line-length)))
         (take-to-column
+            (if (prev-end-is ",") prev
+            (if (prev-end-is "[") plus-prev
+            (if (prev-end-is "{") plus-prev
             (if (< curr-ind minus-prev) minus-prev
             (if (and (>= curr-ind minus-prev) (< curr-ind prev)) prev
-            (if (and (>= curr-ind prev) (< curr-ind plus-prev)) plus-prev 0))))
+            (if (and (>= curr-ind prev) (< curr-ind plus-prev)) plus-prev 0)))))))
         (let ((new-pos (+ current (- (get-current-indent) curr-ind))))
-            (if (>= new-pos  0)
+            (if (>= new-pos 0)
             (move-to-column new-pos)
             (move-to-column 0)))))
 
