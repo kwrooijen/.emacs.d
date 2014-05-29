c11a7909be04d3a8a63f44943b9f70dcb05b4d5f
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2014-05-29 15:42:48 +0200

    Added copyright

--- config/indent-of-doom.el
+++ config/indent-of-doom.el
@@ -1,3 +1,41 @@
+;;; indent-of-doom.el ---
+;;
+;; Filename: indent-of-doom.el
+;; Description: A customizable general purpose indenting mode
+;; Author: Kevin van Rooijen
+;; Maintainer: Kevin van Rooijen
+;; Created: May 25 2014
+;; Version: 0.1.0
+;; Package-Requires: (dash)
+;; URL:
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
 ;;;;;;;;;;;;;;;;;;;;;; Utils ;;;;;;;;;;;;;;;;;;;;;;
 
 (defun what-line-int ()
@@ -213,3 +251,5 @@
 (setq my-doom '())
 
 (provide 'indent-of-doom)
+
+;;; indent-of-doom.el ends here
