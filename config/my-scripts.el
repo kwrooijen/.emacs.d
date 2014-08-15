5392fd44e2d8ebe741720125f989ba7cd4a84fe1
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2014-08-16 01:02:07 +0200

    Moved elixir-guard to elisp

--- config/my-scripts.el
+++ config/my-scripts.el
@@ -12,4 +12,23 @@ while [ $PWD != \"/\" ]; do
 done
 " file command) buff))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;; Elixir ;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq my-elixir-guard-init "
+#!/bin/bash
+
+echo 'group :development do
+  gem \"guard-elixir\"
+end' > Gemfile
+
+echo 'notification :emacs
+guard :elixir do
+  watch(%r{^test/(.*)_test\.exs})
+  watch(%r{^lib/(.+)\.ex$})           { |m| \"test/#{m[1]}_test.exs\" }
+  watch(%r{^test/test_helper.exs$})   { \"test\" }
+end' > Guardfile
+")
+
 (provide 'my-scripts)
