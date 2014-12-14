d11f965937292569e14b24be0574fa7072a46f3e
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2014-12-14 01:12:02 +0100

    Modified colors

--- config/my-colors.el
+++ config/my-colors.el
@@ -7,20 +7,20 @@
 
 (defun sfb(l)
     (mapcar (lambda(list) (set-foreground-background (nth 0 list)(nth 1 list)(nth 2 list) )) l))
-
-(sfb '(
-    (git-gutter+-added "green" "#383838" )
-    (git-gutter+-deleted "red" "#383838" )
-    (git-gutter+-modified "magenta" "#383838" )
-    (git-gutter+-unchanged nil "#383838")
-    (helm-selection "white" "royalblue")
-    (hl-line nil "gray18")
-    (helm-ff-directory "deep sky blue" unspecified)
-    (company-scrollbar-bg   nil "gainsboro")
-    (company-template-field "black" "gainsboro")
-    (company-tooltip        "black" "gainsboro")
-))
 (load-theme 'underwater t)
+(sfb '(
+    (git-gutter+-added          "green" "#383838")
+    (git-gutter+-deleted        "red" "#383838")
+    (git-gutter+-modified       "magenta" "#383838")
+    (git-gutter+-unchanged  nil "#383838")
+    (helm-selection             "white" "royalblue")
+    (helm-ff-directory          "deep sky blue" unspecified)
+    (company-scrollbar-bg   nil "gainsboro")
+    (company-template-field     "black" "gainsboro")
+    (company-tooltip            "black" "gainsboro")
+    (region                     "white" "deep sky blue")
+))
+
 
 (set-face-underline 'hl-line nil)
 
