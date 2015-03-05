545c6413e85b3bd7411db6259bc9cc6e899211c8
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2015-03-05 23:54:32 +0100

    Added function to edit current file as root

--- config/my-functions.el
+++ config/my-functions.el
@@ -1,3 +1,15 @@
+(defun sudo-edit (&optional arg)
+  "Edit currently visited file as root.
+
+With a prefix ARG prompt for a file to visit.
+Will also prompt for a file to visit if current
+buffer is not visiting a file."
+  (interactive "P")
+  (if (or arg (not buffer-file-name))
+      (find-file (concat "/sudo:root@localhost:"
+                         (ido-read-file-name "Find file(as root): ")))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
 (defun ssh-add ()
   (interactive)
   (async-shell-command "ssh-add ~/.ssh/id_rsa"))
