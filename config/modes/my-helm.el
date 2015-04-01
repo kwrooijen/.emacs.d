70d8b7833e4d7c72c7be19bd47aeb610a4974def
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2015-04-01 23:32:11 +0200

    experimental helm settings

--- config/modes/my-helm.el
+++ config/modes/my-helm.el
@@ -72,6 +72,42 @@
 (setq helm-autoresize-max-height 30)
 (setq helm-autoresize-min-height 30)
 
+
+(setq helm-exit-idle-delay 0.1
+      ;; truncate long lines in helm completion
+      helm-truncate-lines t
+      ;; may be overridden if 'ggrep' is in path (see below)
+      helm-grep-default-command
+      "grep -a -d skip %e -n%cH -e %p %f"
+      helm-grep-default-recurse-command
+      "grep -a -d recurse %e -n%cH -e %p %f"
+      ;; do not display invisible candidates
+      helm-quick-update t
+      ;; be idle for this many seconds, before updating in delayed sources.
+      helm-idle-delay 0.01
+      ;; be idle for this many seconds, before updating candidate buffer
+      helm-input-idle-delay 0.01
+      ;; open helm buffer in another window
+      helm-split-window-default-side 'other
+      ;; limit the number of displayed canidates
+      helm-candidate-number-limit 200
+      ;; don't use recentf stuff in helm-ff
+      helm-ff-file-name-history-use-recentf nil
+      ;; move to end or beginning of source when reaching top or bottom
+      ;; of source
+      helm-move-to-line-cycle-in-source t
+      ;; fuzzy matching
+      helm-buffers-fuzzy-matching t
+      helm-semantic-fuzzy-match t
+      helm-imenu-fuzzy-match t
+      helm-completion-in-region-fuzzy-match t
+      ;; Here are the things helm-mini shows, I add `helm-source-bookmarks'
+      ;; here to the regular default list
+      ;; helm-mini-default-sources '(helm-source-buffers-list
+      ;;                             helm-source-recentf
+      ;;                             helm-source-bookmarks)
+      )
+
 ;;==============================================================================
 ;;== Hook
 ;;==============================================================================
