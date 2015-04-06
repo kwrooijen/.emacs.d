4088a3a524e6d55796ee3d82c4cac907980cf555
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2015-04-06 18:48:59 +0200

    fixed typo

--- init.el
+++ init.el
@@ -33,12 +33,12 @@
  '(custom-safe-themes
    (quote
     ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "0f6e58d1814b4138c5a88241f96547d35883cbb3df6cf9ec8ef44856ece04c13" default)))
- '(mode-line-format mode-line-format-backup))
+ '(mode-line-format attic-mode-line-format))
 
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
