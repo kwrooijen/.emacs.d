9e5993456359d86f39e7196442c73f48f481ba8d
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2020-04-21 19:20:45 +0200

    wip

--- init.el
+++ init.el
@@ -161,6 +161,7 @@
 
 (use-package helm-ag
   :straight t
+  :after evil-collection
   :config
   (define-key helm-ag-map (kbd "C-j")
     (lambda ()
@@ -168,7 +169,7 @@
       (helm-next-line)
       (helm-execute-persistent-action)))
   (define-key helm-ag-map (kbd "C-k")
-    (lambda ()
+    (lambda
       (interactive)
       (helm-previous-line)
       (helm-execute-persistent-action))))
@@ -179,7 +180,13 @@
 (use-package helm-ag
   :straight t)
 
+(use-package projectile
+  :straight t
+  :config
+  (projectile-mode))
+
 (use-package helm-projectile
+  :after projectile
   :straight t)
 
 (use-package anzu
@@ -301,6 +308,13 @@
     (backward-word)
     (capitalize-word 1)))
 
+(defun select-minibuffer ()
+ "Make the active minibuffer the selected window."
+ (interactive)
+ (when (active-minibuffer-window)
+   (select-window (active-minibuffer-window))))
+
+
 ;;
 ;; Key bindings
 ;;
@@ -364,113 +378,15 @@
 
  ;; Resume
  "r r" 'helm-resume
- "r y" 'helm-show-kill-ring)
+ "r y" 'helm-show-kill-ring
+ "r b" 'select-minibuffer)
 
 (evil-leader/set-leader "SPC")
 
 
 ;; TODO check these
-;; (anzu-mode
-;; +org-pretty-mode
-;; git-gutter-mode
-;; global-whitespace-newline-mode
-;; global-whitespace-mode
-;; whitespace-newline-mode
-;; whitespace-mode
-;; highlight-parentheses-mode
-;; highlight-quoted-mode
-;; vi-tilde-fringe-mode
-;; goto-address-prog-mode
-;; goto-address-mode
-;; highlight-numbers-mode
-;; smartparens-mode
-;; ws-butler-mode
-;; projectile-mode
-;; gcmh-mode
-;; better-jumper-local-mode
-;; company-search-mode
 ;; company-mode
-;; helm-mode
-;; helm-ff--delete-async-modeline-mode
-;; evil-goggles-mode
-;; evil-escape-mode
-;; org-capture-mode
-;; pcre-mode
-;; hl-todo-mode
-;; org-cdlatex-mode
-;; org-src-mode
-;; org-list-checkbox-radio-mode
-;; orgtbl-mode
-;; org-table-follow-field-mode
-;; org-table-header-line-mode
-;; magit-blame-mode
-;; magit-file-mode
-;; magit-wip-initial-backup-mode
-;; magit-wip-before-change-mode
-;; magit-wip-after-apply-mode
-;; magit-wip-after-save-local-mode
-;; magit-wip-mode
-;; smerge-mode
-;; diff-minor-mode
-;; git-commit-mode
-;; mml-mode
-;; with-editor-mode
-;; which-key-mode
-;; buffer-face-mode
-;; text-scale-mode
-;; global-auto-revert-mode
-;; auto-revert-tail-mode
-;; auto-revert-mode
 ;; solaire-mode
-;; evil-snipe-local-mode
-;; flycheck-mode
 ;; clj-refactor-mode
-;; superword-mode
-;; subword-mode
-;; sgml-electric-tag-pair-mode
-;; paredit-mode
+;; superword-mode ???????????? Maybe
 ;; yas-minor-mode
-;; cider--debug-mode
-;; cider-mode
-;; cider-auto-test-mode
-;; cider-popup-buffer-mode
-;; cider-enlighten-mode
-;; vc-parent-buffer
-;; lispyville-mode
-;; lispy-other-mode
-;; lispy-goto-mode
-;; lispy-mode
-;; outline-minor-mode
-;; helm-migemo-mode
-;; counsel-mode
-;; compilation-minor-mode
-;; compilation-shell-minor-mode
-;; compilation-in-progress
-;; ivy-mode
-;; xref-etags-mode
-;; edebug-mode
-;; override-global-mode
-;; mc-hide-unmatched-lines-mode
-;; rectangular-region-mode
-;; multiple-cursors-mode
-;; +web-wordpress-mode
-;; +web-jekyll-mode
-;; +emacs-lisp-ert-mode
-;; +data-vagrant-mode
-;; undo-tree-visualizer-selection-mode
-;; undo-tree-mode
-;; reveal-mode
-;; flyspell-mode
-;; ispell-minor-mode
-;; ns-auto-titlebar-mode
-;; general-override-local-mode
-;; general-override-mode
-;; eldoc-mode
-;; visible-mode
-;; visual-line-mode
-;; next-error-follow-minor-mode
-;; abbrev-mode
-;; overwrite-mode
-;; auto-fill-function
-;; defining-kbd-macro
-;; isearch-mode)
