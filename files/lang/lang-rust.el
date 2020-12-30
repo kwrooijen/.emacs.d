ba2df515e9c13a3ef45109d225d43884e53ced30
Kevin W. van Rooijen <kevin.van.rooijen@gmail.com>
2020-12-30 23:39:08 +0100

    WIP rust

--- files/lang/lang-rust.el
+++ files/lang/lang-rust.el
@@ -1,10 +1,43 @@
 (use-package toml-mode
   :straight t)
 
+(use-package racer
+  :straight t
+  :hook ((rust-mode . racer-mode)
+         (rust-mode . eldoc-mode)))
+
 (use-package rust-mode
   :straight t
+  :init
+  (setq rust-format-on-save t)
   :config
-  (define-key rust-mode-map (kbd "M-i") 'iedit-mode))
+  (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+  (define-key rust-mode-map (kbd "M-i") 'iedit-mode)
+  (mode-leader-def
+    'normal rust-mode-map
+    "'" 'cargo-process-repeat
+    "cB" 'cargo-process-bench
+    "cb" 'cargo-process-build
+    "cc" 'cargo-process-clean
+    "cd" 'cargo-process-doc
+    "cD" 'cargo-process-doc-open
+    "cn" 'cargo-process-new
+    "ci" 'cargo-process-init
+    "cr" 'cargo-process-run
+    "ce" 'cargo-process-run-example
+    "cR" 'cargo-process-run-bin
+    "cs" 'cargo-process-search
+    "ct" 'cargo-process-test
+    "cu" 'cargo-process-update
+    "cT" 'cargo-process-current-test
+    "cf" 'cargo-process-current-file-tests
+    "cC" 'cargo-process-check
+    "cs" 'cargo-process-clippy
+    "ca" 'cargo-process-add
+    ;; "cr" 'cargo-process-rm
+    "cU" 'cargo-process-upgrade
+    "co" 'cargo-process-outdated
+    "ca" 'cargo-process-audit))
 
 (use-package cargo
   :straight t
