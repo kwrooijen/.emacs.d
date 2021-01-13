707dde441ec2e99ddbcd23bdffac8ba6c0033af6
Kevin W. van Rooijen <kevin.van.rooijen@gmail.com>
2021-01-13 10:20:54 +0100

    wip

--- files/lang/lang-clojure.el
+++ files/lang/lang-clojure.el
@@ -263,4 +263,20 @@
       (format "hiccup-cli --html '%s'"
               (hiccup-paste--clipboard-string))))))
 
+(defun hiccup-paste--clipboard-string ()
+  "Return the currency value of the clipboard as a string."
+  (let ((clipboard-text (gui--selection-value-internal 'CLIPBOARD))
+	(select-enable-clipboard t))
+    (if (and clipboard-text (> (length clipboard-text) 0))
+	(kill-new clipboard-text))
+    (car kill-ring)))
+
+(defun hiccup-paste--paste-as-hiccup ()
+  "Paste the HTML in your clipboard as Hiccup syntax."
+  (interactive)
+  (save-excursion
+    (insert
+     (shell-command-to-string
+      (format "hiccup-cli --html '%s'" (hiccup-paste--clipboard-string))))))
+
 (provide 'lang-clojure)
