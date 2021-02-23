614026fe003e5879a39fbcd3a170c34126c91842
Kevin W. van Rooijen <kevin.van.rooijen@gmail.com>
2021-02-23 22:15:02 +0100

    Add hy-eval-region-stay function

--- files/lang/lang-hy.el
+++ files/lang/lang-hy.el
@@ -7,14 +7,20 @@
   (mode-leader-def
     'normal hy-mode-map
     "eb" 'hy-shell-eval-buffer-stay
+    "er" 'hy-shell-eval-region-stay
     "'" 'run-hy))
 
-(defun hy-shell-eval-last-buffer-stay ()
+(defun hy-shell-eval-buffer-stay ()
   "Eval Hy buffer but don't jump to REPL."
   (interactive)
   (save-window-excursion
-    (hy-shell-eval-last-buffer)))
+    (hy-shell-eval-buffer)))
 
+(defun hy-shell-eval-region-stay ()
+  "Eval Hy buffer but don't jump to REPL."
+  (interactive)
+  (save-window-excursion
+    (hy-shell-eval-region)))
 
 (defun hy-shell-eval-last-sexp-stay ()
   "Eval last Hy expression but don't jump to REPL."
