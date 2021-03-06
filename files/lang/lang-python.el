8edc28f027c43d2500d1d6861fe08a0363e29d84
Kevin W. van Rooijen <kevin.van.rooijen@gmail.com>
2021-03-06 15:35:20 +0100

    Add pytest

--- files/lang/lang-python.el
+++ files/lang/lang-python.el
@@ -12,11 +12,17 @@
 (use-package python-mode
   :straight t
   :config
+  (add-hook* 'python-mode-hook (electric-pair-mode 1))
   (mode-leader-def
     'normal python-mode-map
+    "t" '(:ignore t :which-key "Test")
+    "tt" 'elpy-test-pytest-runner
     "e" '(:ignore t :which-key "Evaluate")
     "bb" 'elpy-shell-goto-buffer
     "ee" 'elpy-shell-send-statement
     "eb" 'elpy-shell-send-buffer))
+
+(use-package python-pytest
+  :straight t)
 
 (provide 'lang-python)
